fa16c697f332a9daaa250131293eeb7720b12206
GayathriSubramani07 <gayathrideveloper03@gmail.com>
2025-02-24 14:44:11 +0530

    Create Feb-24Qualifier.java

---
+++ Feb-24Qualifier.java
@@ -0,0 +1,153 @@
+//Main.java
+
+package com.velox;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+
+		Alien obj=context.getBean(Alien.class);
+
+		Desktop obj1=context.getBean(Desktop.class);
+		obj.code();
+		System.out.println(obj.getAge());
+		 
+        
+
+    }
+} 
+
+//Config.java
+package com.velox;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class Config {
+
+    
+    @Bean
+       public Desktop desk() {
+        return new Desktop();  
+    }
+    
+    
+    @Bean
+       public Laptop lap() {
+        return new Laptop();  
+    }
+    
+    @Bean
+    public Alien alien(@Qualifier("lap") Computer com) {
+    	Alien obj=new Alien();
+    	obj.setAge(30);
+    	obj.setCom(com);
+    	
+    	return obj;
+      }
+}
+
+
+//Alien.java
+package com.velox;
+
+public class Alien {
+	
+	private int age;
+	public Computer com;
+	
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+
+	
+
+	
+	Alien(){
+		System.out.println("Alien object is created");
+	}
+
+
+	public Computer getCom() {
+		return com;
+	}
+
+
+
+
+	public void setCom(Computer com) {
+		this.com = com;
+	}
+
+
+
+
+	public void code() {
+		System.out.println("Hello");
+		com.compiler();
+	}
+}
+
+
+//Computer.java
+package com.velox;
+
+public interface Computer {
+    
+    void compiler();  
+}
+
+//Desktop.java
+
+package com.velox;
+
+public class Desktop implements Computer{
+	
+	Desktop(){
+		System.out.println("Desktop object is created");
+	}
+
+    public void compiler() {
+        
+        System.out.println("Compiler logic implemented in Desktop");
+    }
+	
+
+}
+
+//Laptop.java
+package com.velox;
+
+public class Laptop implements Computer{
+	
+	
+	Laptop(){
+		System.out.println("Laptop object is created");
+	}
+
+  
+    public void compiler() {
+        System.out.println("Compiler logic implemented in Laptop");
+    }
+}
+
+ 
